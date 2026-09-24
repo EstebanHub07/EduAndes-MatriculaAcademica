9e9ac7fd5f7508d807d446f54fe250eaf5d89ef1
EstebanHub07 <williams.v.cardenas@upeu.edu.pe>
2026-09-24 08:06:44 -0500

    Inicio

---
+++ src/main/java/pe/edu/upeu/eduandes/EduandesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.eduandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduandesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduandesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upeu/eduandes/EduandesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.eduandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduandesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
