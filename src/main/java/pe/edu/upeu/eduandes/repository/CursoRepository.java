5438be3ba9940a3092e547ab86110830e78308fd
EstebanHub07 <williams.v.cardenas@upeu.edu.pe>
2026-09-24 10:33:02 -0500

    Agregamos los modulos Carrera y Curso

--- src/main/java/pe/edu/upeu/eduandes/repository/CursoRepository.java
+++ src/main/java/pe/edu/upeu/eduandes/repository/CursoRepository.java
@@ -1,4 +1,29 @@
 package pe.edu.upeu.eduandes.repository;
 
-public interface CursoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pe.edu.upeu.eduandes.entity.Curso;
+
+import java.util.List;
+
+public interface CursoRepository
+        extends JpaRepository<Curso, Long>,
+        JpaSpecificationExecutor<Curso> {
+
+    long countByCodigoIgnoreCase(String codigo);
+
+    long countByCodigoIgnoreCaseAndIdNot(String codigo, Long id);
+
+    long countByCarreraId(Long carreraId);
+
+    List<Curso> findByCarreraIdOrderByNombreAsc(Long carreraId);
+
+    @Query("""
+        SELECT COUNT(d)
+        FROM DetalleMatricula d
+        WHERE d.curso.id = :cursoId
+        """)
+    long contarMatriculasPorCurso(@Param("cursoId") Long cursoId);
 }
--- src/main/java/pe/edu/upeu/eduandes/service/service/CarreraService.java
+++ src/main/java/pe/edu/upeu/eduandes/service/service/CarreraService.java
@@ -1,4 +1,8 @@
 package pe.edu.upeu.eduandes.service.service;
 
-public interface CarreraService {
+import pe.edu.upeu.eduandes.dto.request.CarreraRequestDTO;
+import pe.edu.upeu.eduandes.dto.response.CarreraResponseDTO;
+import pe.edu.upeu.eduandes.service.generic.CrudService;
+
+public interface CarreraService extends CrudService<CarreraRequestDTO, CarreraResponseDTO,Long> {
 }
